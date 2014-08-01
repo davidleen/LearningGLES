180a5adf3db7dd3fe1372fa9376dc25f86d79f08
davidleen <davidleen29@gmailc.om>
2014-08-01 16:14:38 +0800

    nothing

--- assets/shadowtest/vertex.glsl
+++ assets/shadowtest/vertex.glsl
@@ -15,6 +15,7 @@ varying vec4 diffuse;
 varying vec4 specular;
 varying vec2 vTextureCoord;
 varying  vec4 vPosition;
+varying float vDotNL; //光线与法向量的点乘
 //定位光光照计算的方法
 void pointLight(					//定位光光照计算的方法
   in vec3 normal,				//法向量
@@ -24,20 +25,27 @@ void pointLight(					//定位光光照计算的方法
   in vec3 lightLocation,			//光源位置
   in vec4 lightAmbient,			//环境光强度
   in vec4 lightDiffuse,			//散射光强度
-  in vec4 lightSpecular			//镜面光强度
+  in vec4 lightSpecular,			//镜面光强度
+  out float dotNL                //光线与法向量的点乘
 ){
-  ambient=lightAmbient;			//直接得出环境光的最终强度  
+  ambient=lightAmbient;			//直接得出环境光的最终强度
+
+
+   vec4 newPosition=uMMatrix*vec4(aPosition,1);
   vec3 normalTarget=aPosition+normal;	//计算变换后的法向量
-  vec3 newNormal=(uMMatrix*vec4(normalTarget,1)).xyz-(uMMatrix*vec4(aPosition,1)).xyz;
+
+  vec3 newNormal=(uMMatrix*vec4(normalTarget,1)).xyz-newPosition.xyz;
   newNormal=normalize(newNormal); 	//对法向量规格化
+
   //计算从表面点到摄像机的向量
-  vec3 eye= normalize(uCamera-(uMMatrix*vec4(aPosition,1)).xyz);  
+  vec3 eye= normalize(uCamera-newPosition.xyz);
   //计算从表面点到光源位置的向量vp
-  vec3 vp= normalize(lightLocation-(uMMatrix*vec4(aPosition,1)).xyz);  
+  vec3 vp= normalize(lightLocation-newPosition.xyz);
   vp=normalize(vp);//格式化vp
   vec3 halfVector=normalize(vp+eye);	//求视线与光线的半向量    
   float shininess=5.0;				//粗糙度，越小越光滑
   float nDotViewPosition=max(0.0,dot(newNormal,vp)); 	//求法向量与vp的点积与0的最大值
+  dotNL=nDotViewPosition;
   diffuse=lightDiffuse*nDotViewPosition;				//计算散射光的最终强度
   float nDotViewHalfVector=dot(newNormal,halfVector);	//法线与半向量的点积 
   float powerFactor=max(0.0,pow(nDotViewHalfVector,shininess)); 	//镜面反射光强度因子
@@ -49,12 +57,14 @@ void main()
 { 
    gl_Position = uMVPMatrix * vec4(aPosition,1); //根据总变换矩阵计算此次绘制此顶点位置  
    
-   vec4 ambientTemp, diffuseTemp, specularTemp;   //存放环境光、散射光、镜面反射光的临时变量      
-   pointLight(normalize(aNormal),ambientTemp,diffuseTemp,specularTemp,uLightLocation,abientLight,lightDiffuse,lightSpecular);
+   vec4 ambientTemp, diffuseTemp, specularTemp;   //存放环境光、散射光、镜面反射光的临时变量
+    float dotNL;
+   pointLight(normalize(aNormal),ambientTemp,diffuseTemp,specularTemp,uLightLocation,abientLight,lightDiffuse,lightSpecular,dotNL);
    
    ambient=ambientTemp;
    diffuse=diffuseTemp;
    specular=specularTemp;
+   vDotNL=dotNL;
    vTextureCoord = aTexCoor;//将接收的纹理坐标传递给片元着色器
 
 
